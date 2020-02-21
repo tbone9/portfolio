0da002cbf1f0becfd1726ba3cd6366da64191362
Tyler Walker <tylerjwalker@gmail.com>
2020-02-21 10:33:14 -0700

    added break point at 600px and changed nav to header and burger

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,40 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../Nav/style.css'
 
 const Nav = () => {
+
+    const [isNavVisible, setIsNavVisible] = useState(true);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
+
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 600px)");
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
+        return () => {
+            mediaQuery.removeListener(handleMediaQueryChange);
+        };
+    }, [])
+
+    const handleMediaQueryChange = mediaQuery => {
+        if(mediaQuery.matches) {
+            setIsSmallScreen(true);
+        } else {
+            setIsSmallScreen(false);
+        }
+    }
     
         return (
-            <React.Fragment>
+            <header className='header'>
+
+                <div className='logo'>TW</div>
                 
-                <div className='link-group'>
-                    <div className='link'>
-                        <NavLink 
-                        exact
-                        activeClassName='active'
-                        className='navLink'
-                        to={'/'}>
-                            Home</NavLink>
-                    </div>
-                    <div className='link'>
-                        <NavLink 
-                        exact
-                        activeClassName='active'
-                        className='navLink'
-                        to={'/About'}>
-                            About</NavLink>
-                    </div>
-                    <div className='link'>
-                        <NavLink 
-                        exact
-                        activeClassName='active'
-                        className='navLink'
-                        to={'/Projects'}>
-                            Projects</NavLink>
-                    </div>
-                </div>
+                {(!isSmallScreen || !isNavVisible) && (
+                    <nav className='nav'>
+                        <div className='link'>
+                            <NavLink 
+                            exact
+                            activeClassName='active'
+                            className='navLink'
+                            to={'/'}>
+                                Home</NavLink>
+                        </div>
+                        <div className='link'>
+                            <NavLink 
+                            exact
+                            activeClassName='active'
+                            className='navLink'
+                            to={'/About'}>
+                                About</NavLink>
+                        </div>
+                        <div className='link'>
+                            <NavLink 
+                            exact
+                            activeClassName='active'
+                            className='navLink'
+                            to={'/Projects'}>
+                                Projects</NavLink>
+                        </div>
+                    </nav>
+                )}
+                <button onClick={toggleNav} className='burger'>Menu</button>
                 
-            </React.Fragment>
+            </header>
         )
     
 }
